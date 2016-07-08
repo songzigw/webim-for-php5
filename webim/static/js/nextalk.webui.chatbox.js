e81802366692e76378c78c652c8c807306e74d2b
songzigw <songzigw@163.com>
2016-07-08 12:03:51 +0800

    emoji

--- webim/static/js/nextalk.webui.chatbox.js
+++ webim/static/js/nextalk.webui.chatbox.js
@@ -636,6 +636,8 @@ if (!nextalk.webui) {
             return false;
         });
         _this.$input.on('keyup', function(ev) {
+            var _this = this, $input = _this.$input;
+            $input.text(utf16toEmpty($input.text()));
             _this.resizable();
         });
         _this.$input.on('keydown', function(ev) {
@@ -647,6 +649,7 @@ if (!nextalk.webui) {
                     || e.keyCode == 40) {
                 return;
             }
+            
             if (e.keyCode == 13) {
                 e.preventDefault();
                 e.returnValue = false;
@@ -777,7 +780,7 @@ if (!nextalk.webui) {
     ChatBox.prototype.submit = function() {
         var _this = this, $input = _this.$input;
         if ($.trim($input.text()) != '') {
-            _this.sendMsg(utf16toEntities($input.text()));
+            _this.sendMsg(utf16toEmpty($input.text()));
         }
         $input.text('');
         $input.focus();
@@ -954,30 +957,8 @@ if (!nextalk.webui) {
     webui.ChatBox = ChatBox;
 })(nextalk.webim, nextalk.webui);
 
-/**
- * 
- * 用于把用utf16编码的字符转换成实体字符，以供后台存储
- *
- * @param {string} str 将要转换的字符串，其中含有utf16字符将被自动检出
- * @return {string} 转换后的字符串，utf16字符将被转换成&#xxxx;形式的实体字符
- * 
- */ 
-function utf16toEntities(str) {
+function utf16toEmpty(str) {
     // 检测utf16字符正则
     var patt = /[\ud800-\udbff][\udc00-\udfff]/g;
-    str = str.replace(patt, function(char) {
-        var H, L, code;
-        if (char.length === 2) {
-            // 取出高位
-            H = char.charCodeAt(0);
-            // 取出低位
-            L = char.charCodeAt(1);
-            // 转换算法
-            code = (H - 0xD800) * 0x400 + 0x10000 + L - 0xDC00;
-            return "&#" + code + ";";
-        } else {
-            return char;
-        }
-    });
-    return str;
+    return str.replace(patt, "");
 }
--- webim/u.php
+++ webim/u.php
@@ -32,24 +32,92 @@
 	</style>
 </head>
 <body>
-	<div class="textarea" contenteditable="true" id="textarea"></div>
+	<div class="textarea" contenteditable="true" id="textarea">&#128567;</div>
 	<script type="text/javascript">
 	var tArea = document.getElementById('textarea');
 	tArea.focus();
 	tArea.onkeydown = function(ev) {
 	    var e = window.event || ev;
-		alert(e.keyCode);
-		e.returnValue = false;
-	    if (tArea.innerText.length > 10) {
-	        //ev.returnValue = false;
-	    }
-	    if (ev.keyCode == 13) {
-	        //ev.preventDefault();
-            //ev.returnValue = false;
-        }
+	    //alert(ev.keyCode);
+	  	//ev.preventDefault();
+        //ev.returnValue = false;
 	};
-	tArea.onchange = function() {
-		alert()};
+	tArea.onkeyup = function(ev) {
+	    var e = window.event || ev;
+	    tArea.innerHTML = Emot.trans(tArea.innerHTML);
+	};
+
+	function utf16toEmpty(str) {
+	    // 检测utf16字符正则
+	    var patt = /[\ud800-\udbff][\udc00-\udfff]/g;
+	    return str.replace(patt, "");
+	}
+
+	/**
+	 * 
+	 * 用于把用utf16编码的字符转换成实体字符，以供后台存储
+	 *
+	 * @param {string} str 将要转换的字符串，其中含有utf16字符将被自动检出
+	 * @return {string} 转换后的字符串，utf16字符将被转换成&#xxxx;形式的实体字符
+	 * 
+	 */
+	function utf16toEntities(str) {
+	    // 检测utf16字符正则
+	    var patt = /[\ud800-\udbff][\udc00-\udfff]/g;
+	    str = str.replace(patt, function(char) {
+	        var H, L, code;
+	        if (char.length === 2) {
+	            // 取出高位
+	            H = char.charCodeAt(0);
+	            // 取出低位
+	            L = char.charCodeAt(1);
+	            // 转换算法
+	            code = (H - 0xD800) * 0x400 + 0x10000 + L - 0xDC00;
+	            return "&#" + code + ";";
+	        } else {
+	            return char;
+	        }
+	    });
+	    return str;
+	}
+
+	var Emot = {};
+	Emot.ICON2 = [ {
+            "image" : "default/smile.png",
+            "text" : "&#128567;"
+        }, {
+            "image" : "default/smile-big.png",
+            "text" : "[smile_big]"
+        } ];
+    Emot.trans = function(body) {
+        var path = '/webim/';
+        var reg = /\[(.*?)\]/gm;
+        var str = body.replace(reg, function(match) {
+            for (var i = 0; i < Emot.ICON.length; i++) {
+                var icon = Emot.ICON[i];
+                if (icon.text === match) {
+                    return '<img width="24" height="24" ' 
+                        + 'src="' + path + 'imgs/emot/'
+                        + icon.image + '" />';
+                }
+            }
+            return match;
+        });
+        reg = /\&(.*?);/gm;
+        str = body.replace(reg, function(match) {
+            alert(match);
+            for (var i = 0; i < Emot.ICON2.length; i++) {
+                var icon = Emot.ICON2[i];
+                if (icon.text === match) {
+                    return '<img width="24" height="24" ' 
+                        + 'src="' + path + 'imgs/emot/'
+                        + icon.image + '" />';
+                }
+            }
+            return match;
+        });
+        return str;
+    };
 	</script>
 </body>
 </html>
